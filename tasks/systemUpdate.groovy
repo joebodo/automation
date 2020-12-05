0b107fba94734a4a1cc286ac0b460bbb14d5face
odonnell <odonnell@loopintegration.com>
2020-12-05 14:22:01 -0700

    ant updatesystem (without essential data by default)

---
+++ tasks/systemUpdate.groovy
@@ -0,0 +1,13 @@
+// ant updatesystem -Dtenant=<my tenant> -DconfigFile=path/to/my/config.json
+
+task systemUpdate(group: 'update', description: 'system update') {
+	doFirst {
+		def configFile = project.rootDir.toString() + "/system-update.json"
+
+		assert file(configFile).exists(), "produce a system update file from hac"
+
+		ant.properties['tenant'] = 'master'
+		ant.properties['configFile'] = configFile
+		ant.ant antfile: "build.xml", target: "updatesystem", dir: platform_home
+	}
+}
