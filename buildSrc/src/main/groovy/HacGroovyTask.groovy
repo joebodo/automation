d2596d19d57f4eda0face9b48a93a72ed7e49691
odonnell <odonnell@loopintegration.com>
2020-12-16 13:29:45 -0700

    Replace Hac client backend

--- buildSrc/src/main/groovy/HacGroovyTask.groovy
+++ buildSrc/src/main/groovy/HacGroovyTask.groovy
@@ -1,43 +1,52 @@
-package automation
+package automation;
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.Input
+import com.sap.hybris.hac.Configuration
+import com.sap.hybris.hac.HybrisAdministrationConsole
+import com.sap.hybris.hac.scripting.Script
+import com.sap.hybris.hac.scripting.ScriptType
 
-// following is a cleaner approach
-// See: https://github.com/klaushauschild1984/jhac/blob/master/jhac-core/src/main/java/com/sap/hybris/hac/Base.java
+// See: https://github.com/klaushauschild1984/jhac
 
 class HacGroovyTask extends DefaultTask {
 
 	@Input
 	String script = '<none>'
+
 	@Input
 	String settings = 'http://localhost:9001/hac;admin;nimda'
 
 	@TaskAction
 	def postScript() {
 
-		def (url, username, password) = settings.tokenize( ';' )
+		def (url, username, password) = settings.tokenize(';')
 
-		def settings = new Project().with {
-			hacLogin = username
-			hacPassword = password
-			hostHacUrl = url
-			it // Note the explicit mention of it as the return value
-		}
+		def configuration = new Configuration(url, username, password, null)
+		def hac = HybrisAdministrationConsole.hac(configuration)
 
-		def result = new HybrisHacHttpClient().executeGroovyScript(settings, new File(script).text, true)
+		final InputStream inputStream = new BufferedInputStream(new FileInputStream(script))
+
+		def script = Script.builder()
+				.scriptType(ScriptType.groovy)
+				.script(inputStream)
+				.build()
+
+		script.commit = true
+
+		def result = hac.scripting().execute(script)
 
 		if (result.outputText) {
-			println "\nOutput--------------\n"
+			println "\nOutput--------------"
 			println result.outputText
 		}
 		if (result.stacktraceText) {
-			println "\nStacktrace----------\n"
+			println "\nStacktrace----------"
 			println result.stacktraceText
 		}
 		if (result.executionResult) {
-			println "\nResult--------------\n"
+			println "\nResult--------------"
 			println result.executionResult
 		}
 	}
