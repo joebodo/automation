ddf1ce6c7f428c0eca684db599897b83ef8e2e09
Joe O'Donnell <odonnell@loopintegration.com>
2021-04-25 13:27:42 -0600

    Unit testing

--- tasks/alltests.groovy
+++ tasks/alltests.groovy
@@ -1,10 +1,23 @@
-// ant alltests -Dtestclasses.packages="com.southwire.*" -Dtestclasses.suppress.junit.tenant=true -Dtestclasses.web=true
+// ant alltests -Dtestclasses.packages="com.southwire.*"
 
-task allTests(group: 'test', description: 'all tests') {
-	doFirst {
-		ant.properties['testclasses.packages'] = 'com.southwire.test.*'
-		ant.properties['testclasses.suppress.junit.tenant'] = 'true'
-		ant.properties['testclasses.web'] = 'true'
-		ant.ant antfile: 'build.xml', target: 'alltests', dir: platform_home
+task allTests(group: 'test', type: Exec, description: 'all tests') {
+
+	def cmd = [ "$platform_home/apache-ant/bin/ant",
+				'alltests',
+				'-Dtestclasses.packages=com.southwire.*' ]
+
+	workingDir platform_home
+	environment('JAVA_HOME', org.gradle.internal.jvm.Jvm.current().getJavaHome())
+	commandLine cmd
+
+	doLast {
+		def path = "file://$platform_home/../../log/junit/index.html"
+		def os = org.gradle.internal.os.OperatingSystem.current()
+
+		if (os.isWindows()) {
+			exec { commandLine 'cmd', '/c', "start $path" }
+		} else if (os.isMacOsX()) {
+			exec { commandLine 'open', "$path" }
+		}
 	}
 }
--- tasks/webtests.groovy
+++ tasks/webtests.groovy
@@ -1,10 +1,25 @@
 // ant unittests -Dtestclasses.packages="com.southwire.*" -Dtestclasses.suppress.junit.tenant=true -Dtestclasses.web=true
 
-task webTests(group: 'test', description: 'web tests') {
-	doFirst {
-		ant.properties['testclasses.packages'] = 'com.southwire.test.*'
-		ant.properties['testclasses.suppress.junit.tenant'] = 'true'
-		ant.properties['testclasses.web'] = 'true'
-		ant.ant antfile: 'build.xml', target: 'unittests', dir: platform_home
+task webTests(group: 'test', type: Exec, description: 'web tests') {
+
+	def cmd = [ "$platform_home/apache-ant/bin/ant",
+				'unittests',
+				'-Dtestclasses.packages=com.southwire.*',
+				'-Dtestclasses.suppress.junit.tenant=true',
+				'-Dtestclasses.web=true' ]
+
+	workingDir platform_home
+	environment('JAVA_HOME', org.gradle.internal.jvm.Jvm.current().getJavaHome())
+	commandLine cmd
+
+	doLast {
+		def path = "file://$platform_home/../../log/junit/index.html"
+		def os = org.gradle.internal.os.OperatingSystem.current()
+
+		if (os.isWindows()) {
+			exec { commandLine 'cmd', '/c', "start $path" }
+		} else if (os.isMacOsX()) {
+			exec { commandLine 'open', "$path" }
+		}
 	}
 }
