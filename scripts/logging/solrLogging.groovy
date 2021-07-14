eb2ecae4e9bf3ed0c7b5c1223df5d3ba5d6c9a2f
Joe O'Donnell <odonnell@loopintegration.com>
2021-07-14 17:53:08 -0600

    better log toggler

--- scripts/logging/solrLogging.groovy
+++ scripts/logging/solrLogging.groovy
@@ -1,30 +1,16 @@
-import org.apache.logging.log4j.*
-import org.apache.logging.log4j.core.config.*
-import de.hybris.platform.util.logging.log4j2.HybrisLoggerContext
+def toggle(logClass) {
+	def facade = new de.hybris.platform.hac.facade.HacLog4JFacade()
+
+	def loggers = facade.getLoggers()
+	def existing = loggers.find { it.name == logClass }
+
+	def level = existing ? existing.effectiveLevel.name : 'OFF'
+	def flipped = level == "OFF" ? "DEBUG" : "OFF"
+
+	facade.changeLogLevel(logClass, flipped)
+
+	println "$logClass -> $flipped"
+}
 
 toggle('de.hybris.platform.solrfacetsearch.search.impl.LegacyFacetSearchStrategy')
 toggle('de.hybris.platform.solrfacetsearch.search.impl.DefaultFacetSearchStrategy')
-
-def toggle(logClass) {
-	final HybrisLoggerContext loggerCtx = (HybrisLoggerContext) LogManager.getContext(false)
-	final Configuration loggerCfg = loggerCtx.getConfiguration()
-	LoggerConfig loggerConfig = loggerCfg.getLoggers().get(logClass)
-
-	def OFF = Level.getLevel('OFF')
-	def DEBUG = Level.getLevel('DEBUG')
-
-	if (loggerConfig == null) {
-
-		AppenderRef[] refs = [];
-		LoggerConfig createdLoggerConfig = LoggerConfig.createLogger(
-			'true', OFF, logClass, 'true', refs, null, loggerCfg, null)
-
-		loggerCfg.addLogger(logClass, createdLoggerConfig)
-	}
-
-	def logger = loggerCfg.loggers.get(logClass)
-	logger.level = logger.level == OFF ? DEBUG : OFF
-
-	println 'level = ' + logger.level
-	loggerCtx.updateLoggers()
-}
